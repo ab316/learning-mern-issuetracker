8f97cdb10e0885442c91754b50d550c0664b6d12
Abdullah Baig <abdullahbaig456@gmail.com>
2020-01-04 14:48:02 +0100

    Re-arrange code

--- src/app.jsx
+++ src/app.jsx
@@ -1,68 +1,47 @@
 const initialIssues = [
-    {
-        id: 1,
-        status: 'New',
-        owner: 'Abdullah',
-        effort: 5,
-        created: new Date('2020-01-04'),
-        due: undefined,
-        title: 'Error in console when clicking Add'
-    },
-    {
-        id: 2,
-        status: 'Assigned',
-        owner: 'Asad',
-        effort: 14,
-        created: new Date('2019-11-12'),
-        due: new Date('2020-01-01'),
-        title: 'Missing bottom border on panel'
-    }
+    { id: 1, status: 'New', owner: 'Abdullah', effort: 5, created: new Date('2020-01-04'), due: undefined, title: 'Error in console when clicking Add' },
+    { id: 2, status: 'Assigned', owner: 'Asad', effort: 14, created: new Date('2019-11-12'), due: new Date('2020-01-01'), title: 'Missing bottom border on panel' }
 ];
 
 
-class IssueFilter extends React.Component {
+class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = { issues: [] };
+        this.createIssue = this.createIssue.bind(this)
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
     render() {
         return (
-            <div>This is a placeholder for the issue filter</div>
+            <>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={this.state.issues} />
+                <hr />
+                <IssueAdd createIssue={this.createIssue} />
+            </>
         );
     }
-}
 
-function IssueRow(props) {
-    const issue = props.issue;
-    return (
-        <tr>
-            <td>{issue.id}</td>
-            <td>{issue.status}</td>
-            <td>{issue.owner}</td>
-            <td>{issue.effort}</td>
-            <td>{issue.created.toDateString()}</td>
-            <td>{issue.due ? issue.due.toDateString() : ""}</td>
-            <td>{issue.title}</td>
-        </tr>
-    );
-}
+    loadData() {
+        setTimeout(() => {
+            this.setState({ issues: initialIssues });
+        }, 500);
+    }
 
-function IssueTable(props) {
-    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
-    return (
-        <table className="bordered-table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Status</th>
-                    <th>Owner</th>
-                    <th>Effort</th>
-                    <th>Created</th>
-                    <th>Due</th>
-                    <th>Title</th>
-                </tr>
-            </thead>
-            <tbody>
-                {issueRows}
-            </tbody>
-        </table>
-    );
+    createIssue(issue) {
+        issue.id = this.state.issues.length + 1;
+        issue.created = new Date();
+
+        const newIssuesList = this.state.issues.slice();
+        newIssuesList.push(issue);
+        this.setState({ issues: newIssuesList });
+    }
 }
 
 class IssueAdd extends React.Component {
@@ -96,44 +75,49 @@ class IssueAdd extends React.Component {
     }
 }
 
-class IssueList extends React.Component {
-    constructor() {
-        super();
-        this.state = { issues: [] };
-        this.createIssue = this.createIssue.bind(this)
-    }
-
-    componentDidMount() {
-        this.loadData();
-    }
-
-    loadData() {
-        setTimeout(() => {
-            this.setState({ issues: initialIssues });
-        }, 500);
-    }
-
-    createIssue(issue) {
-        issue.id = this.state.issues.length + 1;
-        issue.created = new Date();
-
-        const newIssuesList = this.state.issues.slice();
-        newIssuesList.push(issue);
-        this.setState({ issues: newIssuesList });
-    }
-
+class IssueFilter extends React.Component {
     render() {
         return (
-            <>
-                <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable issues={this.state.issues} />
-                <hr />
-                <IssueAdd createIssue={this.createIssue} />
-            </>
+            <div>This is a placeholder for the issue filter</div>
         );
     }
+}
+
+function IssueTable(props) {
+    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+    return (
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Effort</th>
+                    <th>Created</th>
+                    <th>Due</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {issueRows}
+            </tbody>
+        </table>
+    );
+}
+
+function IssueRow(props) {
+    const issue = props.issue;
+    return (
+        <tr>
+            <td>{issue.id}</td>
+            <td>{issue.status}</td>
+            <td>{issue.owner}</td>
+            <td>{issue.effort}</td>
+            <td>{issue.created.toDateString()}</td>
+            <td>{issue.due ? issue.due.toDateString() : ""}</td>
+            <td>{issue.title}</td>
+        </tr>
+    );
 }
 
 const element = <IssueList />;
