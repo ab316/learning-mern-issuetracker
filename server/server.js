4fce127d6fc1447e869305ee56fbab93a67c9ed9
Abdullah Baig <abdullahbaig456@gmail.com>
2020-01-04 16:09:57 +0100

    Add graphQL server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+const { ApolloServer } = require('apollo-server-express');
 
-const fileServerMiddleware = express.static('public');
-app.use('/', fileServerMiddleware);
+let aboutMessage = "Issue Tracker API v1.0"
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage
+    },
+    Mutation: {
+        setAboutMessage
+    }
+}
+
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers
+});
+
+app.use('/', express.static('public'));
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(3000, () => console.log('App started on port 3000'));
