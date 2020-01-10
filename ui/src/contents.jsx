d9bf5aa960b485088cb008029b3eb8e827dc1998
Abdullah Baig <abdullahbaig456@gmail.com>
2020-01-10 20:32:29 +0100

    UI: Issue edit placeholder

--- ui/src/contents.jsx
+++ ui/src/contents.jsx
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import IssueList from './issueList.jsx';
 import IssueReport from './issueReport.jsx';
+import IssueEdit from './issueEdit.jsx';
 
 const NotFound = () => <h1>Page Not Found</h1>;
 
@@ -12,6 +13,7 @@ export default function Contents() {
             <Redirect exact from="/" to="/issues" />
             <Route path="/issues" component={IssueList} />
             <Route path="/report" component={IssueReport} />
+            <Route path="/edit/:id" component={IssueEdit} />
             <Route component={NotFound} />
         </Switch>
     );
